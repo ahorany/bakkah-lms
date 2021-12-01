98988ee89a1854d71710ec364c21378687d81385
AbedAllah Elhorany <abed_348@hotmail.com>
2021-12-01 12:29:51 +0200

    fix role errors

--- resources/lang/en/admin.php
+++ resources/lang/en/admin.php
@@ -120,9 +120,5 @@ return [
     'learners'=>'Learners',
     'completed_courses'=>'Completed Courses',
     'courses_in_progress'=>'Courses In Progress',
-<<<<<<< HEAD
-=======
-
->>>>>>> 73061c9240ba910ee121f776c5738cf2630f4d35
     'course'=>'course',
 ];
