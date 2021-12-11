4fc82481ef89f83ce9e13c985c5a3c2ee30a6435
Jihad Hamdan <jhamdan@bakkah.net.sa>
2021-12-11 11:53:11 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -138,7 +138,7 @@
                         @else
                             <img src="{{$url}}" height="150px" >
                         @endisset
-
+                        <h3 style="color: #000;">{{$course->trans_title}}</h3>
 
                         <div class="progress">
                             <div style="width: {{$course->pivot->progress??0}}% !important;" class="bar"></div>
@@ -293,8 +293,7 @@
                         <i class="fa fa-angle-left" aria-hidden="true">
                             <svg id="Group_103" data-name="Group 103" xmlns="http://www.w3.org/2000/svg" width="14.836" height="24.835" viewBox="0 0 14.836 24.835">
                                 <path id="Path_99" data-name="Path 99" d="M161.171,218.961a1.511,1.511,0,0,1-1.02-.4l-11.823-10.909a1.508,1.508,0,0,1,0-2.215l11.823-10.912a1.508,1.508,0,0,1,2.045,2.215l-10.625,9.8,10.625,9.8a1.508,1.508,0,0,1-1.025,2.616Z" transform="translate(-147.843 -194.126)" fill="#333"/>
-                              </svg>
-
+                            </svg>
                         </i>
                         <h3>
                             <span class="month"></span>
