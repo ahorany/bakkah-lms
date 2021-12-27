1c7392dcfd29ebf42f90fc74ab8de90f6c3ed763
khaled-elbardawell <elbardawellkhaled@gmail.com>
2021-12-27 15:38:58 +0200

    merge

--- resources/views/pages/file.blade.php
+++ resources/views/pages/file.blade.php
@@ -139,6 +139,7 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+
 @isset($content->upload->file)
     @if($content->post_type == 'video' )
         <script src="https://cdn.plyr.io/3.6.12/plyr.js"></script>
@@ -267,6 +268,8 @@
     </script>
     @endif
 @endisset
+
+
     <script>
         var enabled = "{{$enabled}}";
 
