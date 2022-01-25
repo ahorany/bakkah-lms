6468e0e52dfb142b13e50a8f540e3158cd2ef5dc
Jihad Hamdan <jhamdan@bakkah.net.sa>
2022-01-25 14:07:55 +0200

    fix breadcrump

--- resources/views/training/mails/trainee-mail.blade.php
+++ resources/views/training/mails/trainee-mail.blade.php
@@ -1,78 +1,78 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body style="width: 90%; margin: 0 auto; font-family: sans-serif; text-align:center;">
-    <header style="background: url({{CustomAsset('images/email/Rectangle/1.png')}}); position: relative; height: 130px;">
-        <img src="{{CustomAsset('images/email/top-img.png')}}" alt="" width="100" height="100" style="width: 100px; height:100px; position: absolute; bottom: -50px; left: 50%; transform: translateX(-50%);">
-    </header>
-    <main style="text-align: center; width: 90%; margin: 80px auto 0;">
-        <div style="margin-bottom: 50px;">
-            <h5 style="font-size: 14px; margin: 15px 0;">Dear {{$user->en_name}}</h5>
-            <p style="font-size: 30px; font-weight: bold; margin: 15px 0; text-transform: uppercase; color: #fb4400;">Welcome onboard!</p>
-            <p style="font-size: 15px; margin: 15px 0;">You have registered in LMS. We would like to welcome you and wish you a great learning experience!</p>
-            <hr style="width: 40%; margin: 30px auto;">
-            <p style="font-size: 17px;">This email is intended to provide you with the needed information before the beginning of the course to help you log in our LMS system and deal with the course properly.</p>
-        </div>
-        <div style="margin: 40px 0;">
-            <h3 style="font-size: 20px;">The Learning Management System (LMS)</h3>
-            <p style="font-size: 17px;">The access below is for our LMS system where you’ll find the course and all supporting materials!</p>
-        </div>
-        <hr style="margin: 0 auto;">
-        <div style="padding: 30px 0; width: 90%; margin: 0 auto;">
-            <h3 style="color: #fb4400; font-size: 20px; margin-top: 0;">Here’s your details:</h3>
-            <table border="0" cellspacing="0" width="100%">
-                <tr>
-                    <td width="40" style="text-align:left;">
-                        <img src="{{CustomAsset('images/email/link_icon.png')}}" width="auto" height="20px" alt="" style="margin: 0 10px;">
-                    </td>
-                    <td width="310" style="text-align:left;">
-                        <span style="font-size: 15px;"><a href="{{env('APP_URL')}}" style="text-decoration: none;">{{env('APP_URL')}}</a></span>
-                    </td>
-                    <td width="40" style="text-align:left;">
-                        <img src="{{CustomAsset('images/email/user_icon.png')}}" width="auto" height="20px" alt="" style="margin: 0 10px;">
-                    </td>
-                    <td width="310" style="text-align:left;">
-                        <span style="font-size: 15px;">{{$user->email}}</span>
-                    </td>
-                </tr>
-            </table>
-        </div>
-        <hr style="margin: 0 auto;">
-        <p style="font-size: 17px; margin: 30px 0;">If you face any technical issues and need support, please feel free to contact our Training Department and we’ll get in touch with you in due time! </p>
-        <p style="font-size: 17px; color: #fff; background: #fb4400; text-align: center; border-radius: 6px; padding: 15px;">We hope you enjoy this learning experience and wish you best of luck! </p>
-    </main>
-    <footer style="width: 90%; margin: 0 auto;">
-        <table border="0" cellspacing="0" width="100%">
-            <tr>
-                <td width="350" style="text-align:left;">
-                    <a href="#" style="text-decoration: none; margin-right: 5px;">
-                        <img src="{{CustomAsset('images/email/facebook.png')}}" width="auto" height="25px" alt="">
-                    </a>
-                    <a href="#" style="text-decoration: none; margin-right: 5px;">
-                        <img src="{{CustomAsset('images/email/twitter.png')}}" width="auto" height="25px" alt="">
-                    </a>
-                    <a href="#" style="text-decoration: none; margin-right: 5px;">
-                        <img src="{{CustomAsset('images/email/insta.png')}}" width="auto" height="25px" alt="">
-                    </a>
-                    <a href="#" style="text-decoration: none; margin-right: 5px;">
-                        <img src="{{CustomAsset('images/email/linked-in.png')}}" width="auto" height="25px" alt="">
-                    </a>
-                    <div><small>Phone: +966 920003928</small></div>
-                    <div><small>Privacy Policy | Contact Support</small></div>
-                    <div><small>© 2021 <span style="color: #fb4400;">Bakkah Inc.</span></small></div>
+@include('training.mails.layouts.header')
+
+<main style="text-align: center; width: 90%; margin: 15px auto 0;">
+    <div style="margin: 15px 0">
+        <h5 style="font-size: 20px; margin: 15px 0;">Dear {{$user->en_name}},</h5>
+        <p style="font-size: 24px; font-weight: bold; margin: 15px 0; text-transform: uppercase; color: #fb4400;">Welcome onboard!</p>
+        <p style="font-size: 16px; margin: 15px 0;">You have registered in LMS. We would like to welcome you and wish you a great learning experience!</p>
+        <hr style="width: 40%; margin: 15px auto;">
+        <p style="font-size: 16px;">This email is intended to provide you with the needed information before the beginning of the course to help you log in our LMS system and deal with the course properly.</p>
+    </div>
+
+    <div style="margin: 15px 0; background: #f8f8f8; border-radius: 5px;">
+        <table border="0" style="margin: 0 auto; width: 100%; padding: 25px 0;" cellspacing="0">
+            <tr style="line-height: 0.5; text-align: center;">
+                <td colspan="2">
+                    <div>
+                        <h3 style="font-size: 20px;">Details you need to note</h3>
+                    </div>
                 </td>
-                <td width="350" style="text-align:right;">
-                    <img src="{{CustomAsset('images/email/logo.png')}}" width="auto" height="60px" alt="">
+            </tr>
+            <tr style="line-height: 2; text-align: center;">
+                <td>
+                    <div>
+                        <img src="{{CustomAsset('images/email/d2.png')}}" style="vertical-align: bottom;" width="50" height="auto">
+                        <span style="font-size: 18px">Self Study</span>
+                    </div>
+                </td>
+                <td>
+                    <div>
+                        <img src="{{CustomAsset('images/email/d1.png')}}" style="vertical-align: bottom;" width="50" height="auto">
+                        <span style="font-size: 18px">English</span>
+                    </div>
                 </td>
             </tr>
         </table>
+    </div>
 
-    </footer>
-    <img src="{{CustomAsset('images/email/Rectangle small.png')}}" width="100%" alt="">
-</body>
-</html>
+    <div style="margin: 15px 0;">
+        <h3 style="font-size: 20px;">The Learning Management System (LMS)</h3>
+        <p style="font-size: 16px;">The access below is for our LMS system where you’ll find the course and all supporting materials!</p>
+    </div>
+
+    <hr style="width: 40%; margin: 15px auto;">
+
+    <div style="width: 100%; margin: 15px auto;">
+        <h3 style="color: #000; font-size: 20px;">Here’s your details:</h3>
+        <table border="0" style="margin: 0 auto; width: 100%;" cellspacing="0">
+            <tr style="line-height: 1.5; text-align: center;">
+                <td colspan="2">
+                    <div style="width: 80%; margin: 5px auto;">
+                        <span style="margin-right: 5px;">
+                            <img src="{{CustomAsset('images/email/user_icon.png')}}" width="25" height="auto">
+                        </span>
+                        <span style="font-size: 16px">{{$user->email}}</span>
+                    </div>
+                </td>
+            </tr>
+            <tr style="line-height: 1.5; text-align: center;">
+                <td colspan="2">
+                    <div style="width: 80%; margin: 5px auto;">
+                        <span style="margin-right: 5px;">
+                            <img src="{{CustomAsset('images/email/link_icon.png')}}" width="25" height="auto">
+                        </span>
+                        <span style="font-size: 16px;"><a href="{{env('APP_URL')}}" style="text-decoration: none;">{{env('APP_URL')}}</a></span>
+                    </div>
+                </td>
+            </tr>
+        </table>
+    </div>
+
+    <hr style="width: 40%; margin: 15px auto;">
+
+    <p style="font-size: 16px; margin: 15px 0;">If you face any technical issues and need support, please feel free to contact our Training Department and we’ll get in touch with you in due time! </p>
+
+    <p style="font-size: 16px; color: #fff; background: #fb4400; text-align: center; line-height: 1.5; border-radius: 5px; padding: 10px;">We hope you enjoy this learning experience and wish you best of luck! </p>
+</main>
+
+@include('training.mails.layouts.footer')
