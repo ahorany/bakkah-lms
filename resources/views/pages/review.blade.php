843a60f79a5ccea5b8da3109801a1bacdaf5108d
Jihad Hamdan <jhamdan@bakkah.net.sa>
2022-01-30 12:44:33 +0200

    fix

--- resources/views/pages/review.blade.php
+++ resources/views/pages/review.blade.php
@@ -6,10 +6,25 @@
 
 @section('style')
     <style>
-       .incorrect-radio + .radio-mark:after {
-            border: solid #f00 ;
-           border-width: 0 3px 3px 0;
+        .custom-radio .radio-mark:after{
+            border: none;
+            background: #06ae60;
+            border-radius: 50%;
+            width: 15px !important;
+            height: 15px !important;
+            left: 3px !important;
         }
+       .incorrect-radio + .radio-mark:after {
+            /* border: solid #f00 ; */
+           /* border-width: 0 3px 3px 0; */
+            border: none;
+            background: red;
+            border-radius: 50%;
+            width: 15px !important;
+            height: 15px !important;
+            left: 3px !important;
+        }
+
     </style>
 @endsection
 
@@ -93,13 +108,12 @@ if(!is_null($previous)){
                 </div>
                 <h3 style="padding-right: 15%;">{!! $question->title!!}</h3>
                  @foreach($question->answers as $answer)
-                     <label class="custom-radio"> {{$answer->title}}
+                     <label class="custom-radio @foreach($exam->user_answers as $user_answer) @if($user_answer->id == $answer->id ) @if($answer->check_correct == 0) border_false @else border_true @endif @endif @endforeach"
+                      > {{$answer->title}}
                          <input type="checkbox" disabled="true" @foreach($exam->user_answers as $user_answer) @if($user_answer->id == $answer->id ) @if($answer->check_correct == 0) checked class="incorrect-radio" @else checked @endif   @endif @endforeach>
                          <span class="radio-mark"></span>
                      </label>
                  @endforeach
-
-
 
                     <div class="correct_feedback">
                         <h4 class="mb-0">Your mark in this question: </h4>
@@ -175,19 +189,20 @@ if(!is_null($previous)){
                                 @if($count_correct_answers == $count_correct_user_answers && $count_all_user_answers == $count_correct_answers)
                                 <div class="icon correct">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20.248" height="15.247" viewBox="0 0 20.248 15.247">
-                                        <path id="Path_121" data-name="Path 121" d="M252.452,339.764l-11,11-6.414-6.414" transform="translate(-233.618 -338.35)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="4"/>
+                                        <path id="Path_121" data-name="Path 121" d="M252.452,339.764l-11,11-6.414-6.414" transform="translate(-233.618 -338.35)" fill="none" stroke="#06ae60" stroke-miterlimit="10" stroke-width="4"/>
                                     </svg>
                                 </div>
                                 @elseif( ($count_correct_answers > $count_correct_user_answers && $count_correct_user_answers !=0) )
                                     <div class="icon empty"></div>
                                 @else
                                     <div class="icon incorrect">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="14.472" height="14.472" viewBox="0 0 14.472 14.472">
+                                          <svg xmlns="http://www.w3.org/2000/svg" width="14.472" height="14.472" viewBox="0 0 14.472 14.472">
                                             <g id="Group_127" data-name="Group 127" transform="translate(-235.537 -259.17)">
-                                                <line id="Line_9" data-name="Line 9" x2="12.738" y2="12.738" transform="translate(236.404 260.037)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="2.453"/>
-                                                <line id="Line_10" data-name="Line 10" x1="12.738" y2="12.738" transform="translate(236.404 260.037)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="2.453"/>
+                                              <line id="Line_9" data-name="Line 9" x2="12.738" y2="12.738" transform="translate(236.404 260.037)" fill="none" stroke="#ff0000" stroke-miterlimit="10" stroke-width="2.453"/>
+                                              <line id="Line_10" data-name="Line 10" x1="12.738" y2="12.738" transform="translate(236.404 260.037)" fill="none" stroke="#ff0000" stroke-miterlimit="10" stroke-width="2.453"/>
                                             </g>
-                                        </svg>
+                                          </svg>
+
                                     </div>
                                 @endif
                             </a>
