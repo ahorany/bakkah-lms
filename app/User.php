0fdf5d808afb4da5c19ffac07b255d6b84a55b7d
khaled-elbardawell <elbardawellkhaled@gmail.com>
2022-02-20 11:52:34 +0200

    api app

--- app/User.php
+++ app/User.php
@@ -11,8 +11,9 @@ use App\Traits\TrashTrait;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
 
     use Notifiable, TrashTrait, ImgTrait, JsonTrait , HasRoles;
@@ -44,6 +45,27 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
 
 
