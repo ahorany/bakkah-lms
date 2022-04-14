511f1108f7a6847b9d3dd50ed927119df8211a1c
khaled-elbardawell <elbardawellkhaled@gmail.com>
2022-04-14 17:05:54 +0300

    fix bugs

--- app/Models/Training/UserBranch.php
+++ app/Models/Training/UserBranch.php
@@ -9,9 +9,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserBranch extends Model
 {
+    use TrashTrait;
+
     protected $table = "user_branches";
     protected $guarded = [];
 
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function users(){
+        return $this->belongsTo(User::class,'user_id');
+    }
 
 
 }
